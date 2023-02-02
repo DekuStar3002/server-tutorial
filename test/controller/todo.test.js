68312a38d4d2c26a970ddca0059ccf9c26605340
Rahul Behera <rahulbehera2002@gmail.com>
2023-02-02 12:37:31 +0530

    feat: add testcases for catch block

--- test/controller/todo.test.js
+++ test/controller/todo.test.js
@@ -31,6 +31,20 @@ describe('Todo Controller', () => {
     });
   }); 
 
+  it('should return error when called getAllTodo', async () => {
+    jest.spyOn(todoService, 'getAllTodos').mockRejectedValue(new Error('Internal Server Error'));
+    const mockReq = {};
+    const mockRes = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+    await todoController.getAllTodo(mockReq, mockRes);
+    expect(mockRes.status).toBeCalledWith(500);
+    expect(mockRes.json).toBeCalledWith({
+      error: 'Internal Server Error'
+    });
+  }); 
+
   it('should return one todo element when called getTodoById', async () => {
     jest.spyOn(todoService, 'getTodoById').mockResolvedValue({
       id: 1,
@@ -56,6 +70,23 @@ describe('Todo Controller', () => {
         createdAt: 'date-time',
         updatedAt: 'date-time'
       }
+    });
+  }); 
+
+  it('should return error when called getTodoById', async () => {
+    jest.spyOn(todoService, 'getTodoById').mockRejectedValue(new Error('Internal Server Error'));
+
+    const mockReq = {
+      params: jest.fn()
+    };
+    const mockRes = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+    await todoController.getTodoById(mockReq, mockRes);
+    expect(mockRes.status).toBeCalledWith(500);
+    expect(mockRes.json).toBeCalledWith({
+      error: 'Internal Server Error'
     });
   }); 
 
@@ -87,6 +118,23 @@ describe('Todo Controller', () => {
     });
   });
 
+  it('should return error when called addTodo', async () => {
+    jest.spyOn(todoService, 'addTodo').mockRejectedValue(new Error('Internal Server Error'));
+
+    const mockReq = {
+      body: jest.fn()
+    };
+    const mockRes = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+    await todoController.addTodo(mockReq, mockRes);
+    expect(mockRes.status).toBeCalledWith(500);
+    expect(mockRes.json).toBeCalledWith({
+      error: 'Internal Server Error'
+    });
+  }); 
+
   it('should return integer when called putTodo', async () => {
     jest.spyOn(todoService, 'updateTodo').mockResolvedValue([ 1 ]);
     const mockReq = {
@@ -101,6 +149,24 @@ describe('Todo Controller', () => {
     expect(mockRes.status).toBeCalledWith(200);
     expect(mockRes.json).toBeCalledWith({
       data: [ 1 ]
+    });
+  });
+
+  it('should return error when called putTodo', async () => {
+    jest.spyOn(todoService, 'updateTodo').mockRejectedValue(new Error('Internal Server Error'));
+
+    const mockReq = {
+      params: jest.fn(),
+      body: jest.fn()
+    };
+    const mockRes = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+    await todoController.putTodo(mockReq, mockRes);
+    expect(mockRes.status).toBeCalledWith(500);
+    expect(mockRes.json).toBeCalledWith({
+      error: 'Internal Server Error'
     });
   });
 
@@ -121,6 +187,24 @@ describe('Todo Controller', () => {
     });
   });
 
+  it('should return error when called patchTodo', async () => {
+    jest.spyOn(todoService, 'updateTodo').mockRejectedValue(new Error('Internal Server Error'));
+
+    const mockReq = {
+      params: jest.fn(),
+      body: jest.fn()
+    };
+    const mockRes = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+    await todoController.patchTodo(mockReq, mockRes);
+    expect(mockRes.status).toBeCalledWith(500);
+    expect(mockRes.json).toBeCalledWith({
+      error: 'Internal Server Error'
+    });
+  });
+  
   it('should return integer when called deleteTodo', async () => {
     jest.spyOn(todoService, 'updateTodo').mockResolvedValue(1);
     const mockReq = {
@@ -134,6 +218,23 @@ describe('Todo Controller', () => {
     expect(mockRes.status).toBeCalledWith(200);
     expect(mockRes.json).toBeCalledWith({
       data: 1
+    });
+  });
+
+  it('should return error when called deleteTodo', async () => {
+    jest.spyOn(todoService, 'deleteTodo').mockRejectedValue(new Error('Internal Server Error'));
+
+    const mockReq = {
+      params: jest.fn(),
+    };
+    const mockRes = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+    await todoController.deleteTodo(mockReq, mockRes);
+    expect(mockRes.status).toBeCalledWith(500);
+    expect(mockRes.json).toBeCalledWith({
+      error: 'Internal Server Error'
     });
   });
 });
